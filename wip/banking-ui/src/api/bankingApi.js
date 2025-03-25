5727e05a86bc450ec03570b517bb95b90572d228
Nishta <nishta2003@gmail.com>
2025-03-26 02:00:17 +0530

    adding api configurations

--- wip/banking-ui/src/api/bankingApi.js
+++ wip/banking-ui/src/api/bankingApi.js
@@ -6,5 +6,5 @@ export const createAccount = (data) => axios.post(`${API_BASE_URL}/accounts`, da
 export const checkBalance = (accountNumber) => axios.get(`${API_BASE_URL}/accounts/${accountNumber}/balance`);
 export const depositMoney = (data) => axios.post(`${API_BASE_URL}/transfers/deposit`, data);
 export const withdrawMoney = (data) => axios.post(`${API_BASE_URL}/transfers/withdraw`, data);
-export const transferMoney = (data) => axios.post(`${API_BASE_URL}/transfer`, data);
+export const transferMoney = (data) => axios.post(`${API_BASE_URL}/transfers`, data);
 export const getTransactions = (accountNumber) => axios.get(`${API_BASE_URL}/transfers/transactions`, { params: { accountNumber } });
